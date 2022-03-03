752684b42717c27c4e339c2717248e28815991a8
moisesrj97 <moisesrj97@gmail.com>
2022-03-03 12:15:44 +0100

    Extract create app route to separate file

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
-const { execSync } = require('child_process');
 
-const createProject = require('./utils/createProject.js');
-const addBabelTestPlugin = require('./utils/addBabelTestPlugin.js');
-const setModuleType = require('./utils/setModuleType.js');
-const installDependencies = require('./utils/installDependencies.js');
-
-const { babelPluginName } = require('./utils/CONSTANTS.js');
+const createApp = require('./routes/createApp.js');
 
 async function main() {
   console.log('Welcome to express-o-matic!');
@@ -27,79 +21,7 @@ async function main() {
 
   switch (mode) {
     case 'Create an express app':
-      // Collect inquirer questions
-      const { moduleType, middleWare, testingTools } = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'moduleType',
-          message: 'What type of module do you want to use?',
-          choices: ['CommonJS', 'ES6 Modules'],
-        },
-        {
-          type: 'checkbox',
-          name: 'middleWare',
-          message: 'What middleware do you want to use?',
-          choices: ['morgan', 'cors', 'helmet'],
-        },
-        {
-          type: 'checkbox',
-          name: 'testingTools',
-          message: 'What testing tools do you want to use?',
-          choices: ['jest', 'supertest'],
-        },
-      ]);
-
-      let babelPlugin = '';
-      if (testingTools.length > 0 && moduleType === 'ES6 Modules') {
-        babelPlugin = babelPluginName;
-      }
-
-      // Install dependencies
-      console.log('Module type:', moduleType);
-      console.log('Dependencies to be installed:');
-      console.log(
-        [...middleWare, ...testingTools, babelPlugin]
-          .map((e) => '- ' + e)
-          .join('\n')
-      );
-
-      const { proceed } = await inquirer.prompt([
-        {
-          type: 'confirm',
-          name: 'proceed',
-          message: 'Do you want to proceed? (Y/n)',
-          default: true,
-        },
-      ]);
-
-      if (proceed) {
-        // Create package.json with description
-        createProject();
-
-        // Define package.json module type
-        if (moduleType === 'CommonJS') {
-          setModuleType('commonjs');
-        } else {
-          setModuleType('module');
-        }
-
-        if (testingTools.length > 0) {
-          execSync('npm set-script test jest');
-        }
-
-        execSync('npm set-script start "node index.js"');
-
-        // Configure testing if es6 modules selected and test utils selected
-        if (testingTools.length > 0 && moduleType === 'ES6 Modules') {
-          addBabelTestPlugin();
-        }
-
-        // Install dependencies
-        installDependencies([...middleWare, ...testingTools, babelPlugin]);
-      } else {
-        console.log('Aborting...');
-      }
-
+      await createApp();
       break;
     case 'Create a CRUD route for my express app':
       console.log('Creating a CRUD route for my express app...');
---
+++ routes/createApp.js
@@ -0,0 +1,86 @@
+const { execSync } = require('child_process');
+
+const createProject = require('../utils/createProject.js');
+const addBabelTestPlugin = require('../utils/addBabelTestPlugin.js');
+const setModuleType = require('../utils/setModuleType.js');
+const installDependencies = require('../utils/installDependencies.js');
+
+const { babelPluginName } = require('../utils/CONSTANTS.js');
+const inquirer = require('inquirer');
+
+const main = async () => {
+  // Collect inquirer questions
+  const { moduleType, middleWare, testingTools } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'moduleType',
+      message: 'What type of module do you want to use?',
+      choices: ['CommonJS', 'ES6 Modules'],
+    },
+    {
+      type: 'checkbox',
+      name: 'middleWare',
+      message: 'What middleware do you want to use?',
+      choices: ['morgan', 'cors', 'helmet'],
+    },
+    {
+      type: 'checkbox',
+      name: 'testingTools',
+      message: 'What testing tools do you want to use?',
+      choices: ['jest', 'supertest'],
+    },
+  ]);
+
+  let babelPlugin = '';
+  if (testingTools.length > 0 && moduleType === 'ES6 Modules') {
+    babelPlugin = babelPluginName;
+  }
+
+  // Install dependencies
+  console.log('Module type:', moduleType);
+  console.log('Dependencies to be installed:');
+  console.log(
+    [...middleWare, ...testingTools, babelPlugin]
+      .map((e) => '- ' + e)
+      .join('\n')
+  );
+
+  const { proceed } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'proceed',
+      message: 'Do you want to proceed? (Y/n)',
+      default: true,
+    },
+  ]);
+
+  if (proceed) {
+    // Create package.json with description
+    createProject();
+
+    // Define package.json module type
+    if (moduleType === 'CommonJS') {
+      setModuleType('commonjs');
+    } else {
+      setModuleType('module');
+    }
+
+    if (testingTools.length > 0) {
+      execSync('npm set-script test jest');
+    }
+
+    execSync('npm set-script start "node index.js"');
+
+    // Configure testing if es6 modules selected and test utils selected
+    if (testingTools.length > 0 && moduleType === 'ES6 Modules') {
+      addBabelTestPlugin();
+    }
+
+    // Install dependencies
+    installDependencies([...middleWare, ...testingTools, babelPlugin]);
+  } else {
+    console.log('Aborting...');
+  }
+};
+
+module.exports = main;
