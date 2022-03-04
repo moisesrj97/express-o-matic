59d5365cede3585709191012c5c9572fe2cd2657
moisesrj97 <moisesrj97@gmail.com>
2022-03-04 09:41:31 +0100

    Refactor create-controller to string literals

--- helpers/create-controller-file.js
+++ helpers/create-controller-file.js
@@ -14,61 +14,82 @@ const main = (
   if (!dbExtract) {
     // Create controller file content
     if (moduleType === 'module') {
-      // Get all
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const getAll${capitalizedResourceName}s = (req, res) => res.status(200).send("This route get all ${capitalizedResourceName}s");\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const get${capitalizedResourceName} = (req, res) => res.status(200).send("This route get ${capitalizedResourceName} with id " + req.params.id);\n\n`
-      );
-      // Create
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const create${capitalizedResourceName} = (req, res) => res.status(200).send("This route create a ${capitalizedResourceName} with value " + JSON.stringify(req.body));\n\n`
-      );
-      // Update
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const update${capitalizedResourceName} = (req, res) => res.status(200).send("This route update ${capitalizedResourceName} with id " + req.params.id + " and value " + JSON.stringify(req.body));\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const delete${capitalizedResourceName} = (req, res) => res.status(200).send("This route delete ${capitalizedResourceName} with id " + req.params.id);\n\n`
+        `export const getAll${capitalizedResourceName}s = (req, res) => {
+  res
+    .status(200)
+    .send("This route get all ${capitalizedResourceName}s");
+};
+
+export const get${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route get ${capitalizedResourceName} with id " + req.params.id);
+};
+
+export const create${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route create a ${capitalizedResourceName} with value " + JSON.stringify(req.body));
+};
+
+export const update${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route update ${capitalizedResourceName} with id " + req.params.id + " and value " + JSON.stringify(req.body));
+};
+
+const delete${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route delete ${capitalizedResourceName} with id " + req.params.id);
+};
+
+`
       );
     } else {
-      // Get all
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `const getAll${capitalizedResourceName}s = (req, res) => res.status(200).send("This route get all ${capitalizedResourceName}s");\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const get${capitalizedResourceName} = (req, res) => res.status(200).send("This route get ${capitalizedResourceName} with id " + req.params.id);\n\n`
-      );
-      // Create
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const create${capitalizedResourceName} = (req, res) => res.status(200).send("This route create a ${capitalizedResourceName} with value " + JSON.stringify(req.body));\n\n`
-      );
-      // Update
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const update${capitalizedResourceName} = (req, res) => res.status(200).send("This route update ${capitalizedResourceName} with id " + req.params.id + " and value " + JSON.stringify(req.body));\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const delete${capitalizedResourceName} = (req, res) => res.status(200).send("This route delete ${capitalizedResourceName} with id " + req.params.id);\n\n`
-      );
-      // Export
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `module.exports = {\n  getAll${capitalizedResourceName}s,\n  get${capitalizedResourceName},\n  create${capitalizedResourceName},\n  update${capitalizedResourceName},\n  delete${capitalizedResourceName}\n};\n\n`
+        `const getAll${capitalizedResourceName}s = (req, res) => {
+  res
+    .status(200)
+    .send("This route get all ${capitalizedResourceName}s");
+};
+
+const get${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route get ${capitalizedResourceName} with id " + req.params.id);
+};
+
+const create${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route create a ${capitalizedResourceName} with value " + JSON.stringify(req.body));
+};
+
+const update${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route update ${capitalizedResourceName} with id " + req.params.id + " and value " + JSON.stringify(req.body));
+};
+
+const delete${capitalizedResourceName} = (req, res) => {
+  res
+    .status(200)
+    .send("This route delete ${capitalizedResourceName} with id " + req.params.id);
+};
+
+module.exports = {
+  getAll${capitalizedResourceName}s,
+  get${capitalizedResourceName},
+  create${capitalizedResourceName},
+  update${capitalizedResourceName},
+  delete${capitalizedResourceName},
+};
+
+`
       );
     }
   } else {
@@ -79,62 +100,62 @@ const main = (
 
     if (moduleType === 'module') {
       // Create db file content
-      // Get all
       appendFileSync(
         `db/${kebabCaseResourceName}.db.js`,
-        `export const getAll${capitalizedResourceName}sDB = async () => "This route get all ${capitalizedResourceName}s";\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `export const get${capitalizedResourceName}DB = async (id) =>  "This route get ${capitalizedResourceName} with id " + id;\n\n`
-      );
-      // Create
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `export const create${capitalizedResourceName}DB = async (new${capitalizedResourceName}) =>  "This route create a ${capitalizedResourceName} with value " + JSON.stringify(new${capitalizedResourceName});\n\n`
-      );
-      // Update
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `export const update${capitalizedResourceName}DB = async (id, updated${capitalizedResourceName}) => "This route updated${capitalizedResourceName} with id " + id + " and new value " + JSON.stringify(updated${capitalizedResourceName});\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `export const delete${capitalizedResourceName}DB = async (id) =>  "This route delete ${capitalizedResourceName} with id " + id;\n\n`
+        `export const getAll${capitalizedResourceName}sDB = async () => {
+  return "This route get all ${capitalizedResourceName}s";
+};
+
+export const get${capitalizedResourceName}DB = async (id) => {
+  return "This route get ${capitalizedResourceName} with id " + id;
+};
+
+export const create${capitalizedResourceName}DB = async (new${capitalizedResourceName}) => {
+  return "This route create a ${capitalizedResourceName} with value " + JSON.stringify(new${capitalizedResourceName});
+};
+
+export const update${capitalizedResourceName}DB = async (id, updated${capitalizedResourceName}) => {
+  return "This route updated${capitalizedResourceName} with id " + id + " and new value " + JSON.stringify(updated${capitalizedResourceName});
+};
+
+export const delete${capitalizedResourceName}DB = async (id) => {
+  return "This route delete ${capitalizedResourceName} with id " + id;
+};
+`
       );
     } else {
       // Create db file content
-      // Get all
       appendFileSync(
         `db/${kebabCaseResourceName}.db.js`,
-        `const getAll${capitalizedResourceName}sDB = async () => "This route get all ${capitalizedResourceName}s";\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `const get${capitalizedResourceName}DB = async (id) => "This route get ${capitalizedResourceName} with id " + id;\n\n`
-      );
-      // Create
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `const create${capitalizedResourceName}DB = async (new${capitalizedResourceName}) =>  "This route create a ${capitalizedResourceName} with value " + JSON.stringify(new${capitalizedResourceName});\n\n`
-      );
-      // Update
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `const update${capitalizedResourceName}DB = async (id, updated${capitalizedResourceName}) => "This route updated${capitalizedResourceName} with id " + id + " and new value " + JSON.stringify(updated${capitalizedResourceName});\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `const delete${capitalizedResourceName}DB = async (id) => "This route delete ${capitalizedResourceName} with id " + id;\n\n`
-      );
-      // Exports
-      appendFileSync(
-        `db/${kebabCaseResourceName}.db.js`,
-        `module.exports = {\n  getAll${capitalizedResourceName}sDB,\n  get${capitalizedResourceName}DB,\n  create${capitalizedResourceName}DB,\n  update${capitalizedResourceName}DB,\n  delete${capitalizedResourceName}DB\n};\n\n`
+        `const getAll${capitalizedResourceName}sDB = async () => {
+  return "This route get all ${capitalizedResourceName}s";
+};
+
+const get${capitalizedResourceName}DB = async (id) => {
+  return "This route get ${capitalizedResourceName} with id " + id;
+};
+
+const create${capitalizedResourceName}DB = async (new${capitalizedResourceName}) => {
+  return "This route create a ${capitalizedResourceName} with value " + JSON.stringify(new${capitalizedResourceName});
+};
+
+const update${capitalizedResourceName}DB = async (id, updated${capitalizedResourceName}) => {
+  return "This route updated${capitalizedResourceName} with id " + id + " and new value " + JSON.stringify(updated${capitalizedResourceName});
+};
+
+const delete${capitalizedResourceName}DB = async (id) => {
+  return "This route delete ${capitalizedResourceName} with id " + id;
+};
+
+module.exports = {
+  getAll${capitalizedResourceName}sDB,
+  get${capitalizedResourceName}DB,
+  create${capitalizedResourceName}DB,
+  update${capitalizedResourceName}DB,
+  delete${capitalizedResourceName}DB,
+};
+
+`
       );
     }
 
@@ -142,73 +163,91 @@ const main = (
     if (moduleType === 'module') {
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `import { getAll${capitalizedResourceName}sDB, get${capitalizedResourceName}DB, create${capitalizedResourceName}DB, update${capitalizedResourceName}DB, delete${capitalizedResourceName}DB } from "../db/${kebabCaseResourceName}.db.js";\n\n`
+        `import {
+  getAll${capitalizedResourceName}sDB,
+  get${capitalizedResourceName}DB, 
+  create${capitalizedResourceName}DB, 
+  update${capitalizedResourceName}DB, 
+  delete${capitalizedResourceName}DB, 
+} from "../db/${kebabCaseResourceName}.db.js";\n\n`
       );
     } else {
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `const { getAll${capitalizedResourceName}sDB, get${capitalizedResourceName}DB, create${capitalizedResourceName}DB, update${capitalizedResourceName}DB, delete${capitalizedResourceName}DB } = require("../db/${kebabCaseResourceName}.db.js");\n\n`
+        `const { 
+  getAll${capitalizedResourceName}sDB,
+  get${capitalizedResourceName}DB, 
+  create${capitalizedResourceName}DB, 
+  update${capitalizedResourceName}DB, 
+  delete${capitalizedResourceName}DB, 
+} = require("../db/${kebabCaseResourceName}.db.js");\n\n`
       );
     }
 
     if (moduleType === 'module') {
       // Create controller file content
-      // Get all
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const getAll${capitalizedResourceName}s = (req, res) => getAll${capitalizedResourceName}sDB().then((data) => res.status(200).send(data));\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const get${capitalizedResourceName} = (req, res) => get${capitalizedResourceName}DB(req.params.id).then((data) => res.status(200).send(data));\n\n`
-      );
-      // Create
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const create${capitalizedResourceName} = (req, res) => create${capitalizedResourceName}DB(req.body).then((data) => res.status(201).send(data));\n\n`
-      );
-      // Update
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const update${capitalizedResourceName} = (req, res) => update${capitalizedResourceName}DB(req.params.id, req.body).then((data) => res.status(200).send(data));\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `export const delete${capitalizedResourceName} = (req, res) => delete${capitalizedResourceName}DB(req.params.id).then(() => res.status(204).send());\n\n`
+        `export const getAll${capitalizedResourceName}s = (req, res) => {
+  getAll${capitalizedResourceName}sDB()
+    .then((data) => res.status(200).send(data));
+};
+
+export const get${capitalizedResourceName} = (req, res) => {
+  get${capitalizedResourceName}DB(req.params.id)
+    .then((data) => res.status(200).send(data));
+};
+
+export const create${capitalizedResourceName} = (req, res) => {
+  create${capitalizedResourceName}DB(req.body)
+    .then((data) => res.status(201).send(data));
+};
+
+export const update${capitalizedResourceName} = (req, res) => {
+  update${capitalizedResourceName}DB(req.params.id, req.body)
+    .then((data) => res.status(200).send(data));
+};
+
+export const delete${capitalizedResourceName} = (req, res) => {
+  delete${capitalizedResourceName}DB(req.params.id)
+    .then(() => res.status(204).send());
+};
+
+`
       );
     } else {
       // Create controller file content
-      // Get all
       appendFileSync(
         `controllers/${kebabCaseResourceName}.controller.js`,
-        `const getAll${capitalizedResourceName}s = (req, res) => getAll${capitalizedResourceName}sDB().then((data) => res.status(200).send(data));\n\n`
-      );
-      // Get by id
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const get${capitalizedResourceName} = (req, res) => get${capitalizedResourceName}DB(req.params.id).then((data) => res.status(200).send(data));\n\n`
-      );
-      // Create
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const create${capitalizedResourceName} = (req, res) => create${capitalizedResourceName}DB(req.body).then((data) => res.status(201).send(data));\n\n`
-      );
-      // Update
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const update${capitalizedResourceName} = (req, res) => update${capitalizedResourceName}DB(req.params.id, req.body).then((data) => res.status(200).send(data));\n\n`
-      );
-      // Delete
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `const delete${capitalizedResourceName} = (req, res) => delete${capitalizedResourceName}DB(req.params.id).then(() => res.status(204).send());\n\n`
-      );
-      // Export
-      appendFileSync(
-        `controllers/${kebabCaseResourceName}.controller.js`,
-        `module.exports = {\n  getAll${capitalizedResourceName}s,\n  get${capitalizedResourceName},\n  create${capitalizedResourceName},\n  update${capitalizedResourceName},\n  delete${capitalizedResourceName}\n};\n\n`
+        `const getAll${capitalizedResourceName}s = (req, res) => {
+  getAll${capitalizedResourceName}sDB().then((data) => res.status(200).send(data));
+};
+
+const get${capitalizedResourceName} = (req, res) => {
+  get${capitalizedResourceName}DB(req.params.id).then((data) => res.status(200).send(data));
+};
+
+const create${capitalizedResourceName} = (req, res) => {
+  create${capitalizedResourceName}DB(req.body).then((data) => res.status(201).send(data));
+};
+
+const update${capitalizedResourceName} = (req, res) => {
+  update${capitalizedResourceName}DB(req.params.id, req.body).then((data) => res.status(200).send(data));
+};
+
+const delete${capitalizedResourceName} = (req, res) => {
+  delete${capitalizedResourceName}DB(req.params.id).then(() => res.status(204).send());
+};
+
+module.exports = {
+  getAll${capitalizedResourceName}s,
+  get${capitalizedResourceName},
+  create${capitalizedResourceName},
+  update${capitalizedResourceName},
+  delete${capitalizedResourceName},
+};
+
+`
       );
     }
   }
