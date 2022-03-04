5213afee9da12695636857cc36810c177353496d
moisesrj97 <moisesrj97@gmail.com>
2022-03-04 10:03:25 +0100

    Fix imports

--- routes/create-middleware.js
+++ routes/create-middleware.js
@@ -1,21 +1,21 @@
 const inquirer = require('inquirer');
 const processResourceName = require('../helpers/process-resource-name.js');
 
-const { readFileSync, writeFileSync, mkdirSync } = require('fs');
+const { readFileSync, writeFileSync, mkdirSync, existsSync } = require('fs');
 
-const main = () => {
+const main = async () => {
   let { middlewareName } = await inquirer.prompt([
     { type: 'input', message: 'Middleware name:', name: 'middlewareName' },
   ]);
 
   let { kebabCaseResourceName, camelCaseResourceName } =
-    processResourceName(resourceName);
+    processResourceName(middlewareName);
 
   // Check module type and testing
   const moduleType = JSON.parse(readFileSync('package.json')).type;
   const dependencies = JSON.parse(readFileSync('package.json')).dependencies;
 
-  if (!readFileSync('middlewares')) {
+  if (!existsSync('middlewares')) {
     mkdirSync('middlewares');
   }
 
@@ -23,30 +23,31 @@ const main = () => {
     writeFileSync(
       `middlewares/${kebabCaseResourceName}.middleware.js`,
       `export const ${camelCaseResourceName}Middleware = (req, res, next) => {
-  try{
+  try {
     //Do something
     next();
-  }catch(err){
+  } catch (err) {
     next(err);
-  };
+  }
 };
 
 `
     );
-  }else{
+  } else {
     writeFileSync(
-        `middlewares/${kebabCaseResourceName}.middleware.js`,
-        `const ${camelCaseResourceName}Middleware = (req, res, next) => {
-  try{
+      `middlewares/${kebabCaseResourceName}.middleware.js`,
+      `const ${camelCaseResourceName}Middleware = (req, res, next) => {
+  try {
     //Do something
     next();
-  }catch(err){
+  } catch (err) {
     next(err);
-  };
+  }
 };
 
-module.exports = ${camelCaseResourceName};`
+module.exports = ${camelCaseResourceName};
 `
+    );
   }
 };
 
